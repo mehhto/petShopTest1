9cd4f51fcbb47ca91a0141483a2c9ad5be38bd2e
mehhto <mehhto@gmail.com>
2020-12-03 19:21:32 +0100

    Added Allure reports description

--- src/test/java/tests/FailedLoginTest.java
+++ src/test/java/tests/FailedLoginTest.java
@@ -1,13 +1,20 @@
 package tests;
 
+import io.qameta.allure.*;
 import org.testng.annotations.Test;
 import page.objects.LandingPage;
+
 import static org.testng.Assert.assertEquals;
 
-public class FailedLoginTest extends TestBase{
+public class FailedLoginTest extends TestBase {
 
+    @Issue("DEFECT-1")
+    @TmsLink("ID-1")
+    @Severity(SeverityLevel.NORMAL)
     @Test
-    public void failedLoginTest(){
+    @Description("The goal of this test is to log in using not proper username and password" +
+            " and check if warning message Invalid username or password is displayed")
+    public void failedLoginTest() {
         LandingPage landingPage = new LandingPage();
         String messageToCheck = landingPage.clickOnEnterShopLink()
                 .clickOnLoginPageLink()
--- src/test/java/tests/FishBuyTest.java
+++ src/test/java/tests/FishBuyTest.java
@@ -1,13 +1,21 @@
 package tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.TmsLink;
 import org.testng.annotations.Test;
-import page.objects.*;
+import page.objects.LandingPage;
 
 import static org.testng.Assert.assertEquals;
 
 public class FishBuyTest extends TestBase {
 
+    @TmsLink("ID-2")
+    @Severity(SeverityLevel.CRITICAL)
     @Test
+    @Description("The goal of this test is add to cart a product without login " +
+            "and check if warning message to login is displayed.")
     public void fishBuyTest() {
 
         LandingPage landingPage = new LandingPage();
